cb3e549c64e660db21548b5f52975764d7078e96
Sudipta Adhikary <58800767+Sudipta065@users.noreply.github.com>
2020-12-01 14:59:47 +0600

    An e-commerce cart using redux

---
+++ src/components/CartContainer.js
@@ -0,0 +1,57 @@
+import React, { useEffect } from "react";
+import CartItem from "./CartItem";
+import { connect } from "react-redux";
+
+import { CLEAR_CART, GET_TOTALS, TOGGLE_AMOUNT } from "../components/actions";
+
+const CartContainer = ({ cart = [], total, dispatch }) => {
+  useEffect(() => {
+    dispatch({ type: GET_TOTALS });
+  }, [cart, dispatch]);
+  if (cart.length === 0) {
+    return (
+      <section className="cart">
+        <header>
+          <h2>My Cart</h2>
+          <h4 className="empty-cart">is currently empty</h4>
+        </header>
+      </section>
+    );
+  }
+  return (
+    <section className="cart">
+      <header>
+        <h2>My Cart </h2>
+      </header>
+      <article>
+        {cart.map((item) => {
+          return <CartItem key={item.id} {...item} />;
+        })}
+      </article>
+      <footer>
+        <hr />
+        <div className="cart-total">
+          <h4>
+            total <span>{total}</span>
+          </h4>
+        </div>
+        <button
+          className="btn clear-btn"
+          onClick={() => {
+            dispatch({ type: CLEAR_CART });
+          }}
+        >
+          clear cart
+        </button>
+      </footer>
+    </section>
+  );
+};
+const mapStateToProps = (store) => {
+  const { cart, total } = store;
+  return {
+    cart,
+    total,
+  };
+};
+export default connect(mapStateToProps)(CartContainer);
